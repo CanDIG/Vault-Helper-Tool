5ee820c87868bff3192180200707781135ffa6d3
Zamm178 <laibazaman360@gmail.com>
2022-03-14 23:47:45 -0400

    added new version of mock

---
+++ docs/cli/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+	"sort"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "Vault Helper Tool CLI",
+		Usage: "Lets user read, update and list user information.",
+		Commands: []*cli.Command{
+			{
+				Name:    "write",
+				Aliases: []string{"w"},
+				Usage:   "update user information by overwriting (provide 1 argument - name of json file)",
+				Action: func(c *cli.Context) error {
+					updateUserInfo(c.Args().Get(0))
+					return nil
+				},
+			},
+			{
+				Name:    "read",
+				Aliases: []string{"r"},
+				Usage:   "read information for user (provide 1 argument - name of user to update)",
+				Action: func(c *cli.Context) error {
+					readUserInfo(c.Args().Get(0), true)
+					return nil
+				},
+			},
+			{
+				Name:    "list",
+				Aliases: []string{"l"},
+				Usage:   "list all users and their data (no arguments needed)",
+				Action: func(c *cli.Context) error {
+					listUserInfo(true)
+					return nil
+				},
+			},
+		},
+	}
+
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+	nextCommands()
+}
---
+++ docs/cli/userInfo.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+// NOTE: this will not be useful once backend is implemented.
+var userArray Users
+
+// Users struct which contains
+// an array of users
+type Users struct {
+	Users []User
+}
+
+// User struct which contains a name and metadata for this basic example
+type User struct {
+	Name     string                 `json:"name"`
+	Metadata map[string]interface{} `json:"metadata"`
+}
+
+// Used to write metadata to vault
+func updateUserInfo(jsonName string) {
+	jsonFile, err := os.Open(jsonName + ".json")
+	if err != nil {
+		fmt.Println("File provided does not exist: ", err)
+	}
+
+	byteValue, parseErr := ioutil.ReadAll(jsonFile)
+	if parseErr != nil {
+		fmt.Println("Error parsing data: ", parseErr)
+	}
+
+	var value User
+	marshErr := json.Unmarshal([]byte(byteValue), &value)
+	if marshErr != nil {
+		fmt.Println("Error using unmarshal: ", marshErr)
+	}
+	userArray.Users = append(userArray.Users, value)
+	jsonFile.Close()
+}
+
+// Used to read metadata from Vault
+func readUserInfo(name string, fromCli bool) {
+	if !fromCli {
+		inVault := false
+		for _, v := range userArray.Users {
+			if v.Name == name {
+				inVault = true
+				fmt.Println(v)
+			}
+		}
+		if !inVault {
+			fmt.Println("User not in Vault")
+		}
+	} else {
+		secretData := map[string]interface{}{
+			"name": "user",
+			"metadata": map[string]interface{}{
+				"dataset123": 4,
+			},
+		}
+		fmt.Println(secretData)
+	}
+}
+
+func listUserInfo(fromCli bool) {
+	if !fromCli {
+		fmt.Println(userArray.Users)
+	} else {
+		secretData := map[string]interface{}{
+			"name": "user",
+			"metadata": map[string]interface{}{
+				"dataset123": 4,
+			},
+		}
+		fmt.Println(secretData)
+	}
+}
+
+func nextCommands() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		result := scanner.Text()
+		newRes := strings.Split(result, " ")
+		command := newRes[0]
+		if command == "write" {
+			updateUserInfo(newRes[1])
+		} else if command == "read" {
+			readUserInfo(newRes[1], false)
+		} else if command == "list" {
+			listUserInfo(false)
+		} else if command == "exit" || command == "q" {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+}
