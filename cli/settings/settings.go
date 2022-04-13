4e1642d3fa9a4f7ac3ab765921a850f6c314ca81
Zamm178 <laibazaman360@gmail.com>
2022-04-12 20:20:33 -0400

    added method for getting enviroment variable

--- cli/settings/settings.go
+++ cli/settings/settings.go
@@ -1,4 +1,31 @@
 package settings
 
-var VAULT_ADDRESS = "http://127.0.0.1:8200"
-var TOKEN_PATH = "../token.txt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnvironmentVariable(variableName string, defaultName string) string {
+	envVar, ok := os.LookupEnv(variableName)
+	if !ok {
+		err := godotenv.Load(path + ".env")
+		if err != nil {
+			return defaultName
+		}
+		fmt.Println(os.Getenv(envVar))
+		return os.Getenv(envVar)
+	}
+	return envVar
+}
+
+var path, _ = os.Getwd()
+var DEFAULT_VAULT_ADDRESS = "http://127.0.0.1:8200"
+var DEFAULT_TOKEN_PATH = path + "/Vault-Helper-Tool/token.txt"
+
+var VAULT_ADDRESS = GetEnvironmentVariable("VAULT_SERVICE_PUBLIC_URL", DEFAULT_VAULT_ADDRESS)
+
+var TOKEN_PATH = GetEnvironmentVariable("TOKEN_PATH", DEFAULT_TOKEN_PATH)
+
+//var PROGRESS_FILE = goDotEnvVariable("PROGRESS_FILE")
